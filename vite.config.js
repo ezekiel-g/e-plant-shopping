bdbf8fffcd569d6ebbcd5636f946927e97116f67
ezekiel-g <40302800+ezekiel-g@users.noreply.github.com>
2025-04-13 14:23:31 -0400

    Trying new vite settings

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+    base: mode === 'production' ? '/e-plant-shopping/' : '/',
+    plugins: [react()]
+}))
